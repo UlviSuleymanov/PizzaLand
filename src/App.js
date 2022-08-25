49271a6b4aa02f82a97077f5bf3c77888c8b00b9
Ülvi Süleymanov <100337078+UlviSuleymanov@users.noreply.github.com>
2022-08-25 14:45:18 +0400

    organized component structure and added useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import './scss/app.scss';
+import Header from './components/header/Header';
+import Categories from './components/categories/Categories';
+import Sort from './components/sort/Sort';
+import PizzaBlock from './components/pizzablock/PizzaBlock';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Bütün Pizzalar</h2>
+            <div className="content__items">
+              <PizzaBlock title={'Sucuk və Göbələk'} price={10} />
+              <PizzaBlock title={'4 Mövsüm'} price={5} />
+              <PizzaBlock title={'Toyuq Barbekü'} price={8} />
+              <PizzaBlock title={'Deluxe Dönər'} price={10.9} />
+              <PizzaBlock title={'Italyano'} price={16} />
+              <PizzaBlock title={'Amercano'} price={17} />
+              <PizzaBlock title={'Mexicano'} price={15} />
+              <PizzaBlock title={'4 Pendirli'} price={19} />
+              <PizzaBlock title={'Supreme'} price={15} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Hamısı</li>
+        <li>Ətli</li>
+        <li>Vegetarian</li>
+        <li>Qril</li>
+        <li>Acılı</li>
+        <li>Bağlı</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
